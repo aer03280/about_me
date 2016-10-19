98d6e7e2b39369a31f3c9ea979b407ce8bcfc80e
Ashley Ross <ashley.e.ross@gmail.com>
2016-10-18 17:04:11 -0700

    Third Commit, new changes to js

--- app.js
+++ app.js
@@ -3,15 +3,24 @@
 var userName = prompt('Welcome to my page! What is your name?');
 console.log('My visitor\'s name is: ' + userName);
 
-var startQuiz = confirm ('Hello, ' + userName + ', thank you for your interest in my life.  Would you like to take a quiz about me?');
+var startQuiz = confirm ('Hello, ' + userName + ', thank you for taking interest in my life.  Would you like to take a quiz about me?');
 if (startQuiz === true){
-  console.log('Visitor wants to play!');
+  console.log(userName + ' wants to play!');
   var yesPlay = confirm ('Excellent!  Here\'s the first question: Do I have a Biology Degree?');
+  alert ('Yes, ' + userName + ', I do have a Biology Degree.');
   console.log('Do I have a Biology Degree? ' + yesPlay);
 } else {
   console.log('Visitor did not want to play, but I made them anyway.');
   var noPlay = confirm ('Hmm, I think you\'ll enjoy it. Let\'s start anyway.  Here\'s the first question: Do I have a Biology Degree?');
   console.log('Do I have a Biology Degree? ' + noPlay);
+  alert ('No, ' + userName + ', actually I do have a Biology Degree.');
 }
 
-var travelQuiz = confirm ('');
+var travelQuiz = prompt ('Did I visit India this summer?');
+if (travelQuiz.toLowerCase() === 'yes' && 'y'){
+  alert ('Great, ' + userName + '! You were paying attention. Click ok to try another question.');
+  console.log(userName + ' was correct that I traveled to India');
+} else {
+  alert ('Nope, ' + userName + ', I actually did travel to India this summer. Click ok to try another question.');
+  console.log(userName, ' was incorrect.  I did travel to India');
+}
